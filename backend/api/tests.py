be7fbeec595818ce500a392210ba88f401542267
Demiat <demiat@mail.ru>
2025-05-20 12:42:35 +0300

    to review 2

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from django.test import TestCase
 from rest_framework.test import APIClient
 
-from users.models import User
+from recipes.models import User
 
 
 class FoodgramAPITestCase(TestCase):
